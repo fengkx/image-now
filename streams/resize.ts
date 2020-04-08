0535cc54fe53a99ddd1d251d9848adbf096ea992
fengkx <liangkx8237@gmail.com>
2020-04-08 20:30:20 +0800

    using fill mode

--- streams/resize.ts
+++ streams/resize.ts
@@ -4,5 +4,5 @@ export default (
   width: number | undefined,
   height: number | undefined
 ): NodeJS.ReadableStream => {
-  return sharp().resize({ width, height });
+  return sharp().resize({ width, height, fit: 'fill' });
 };
