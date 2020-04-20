efa6718242fd283a1d0854f5cdcb701521eea9e8
fengkx <liangkx8237@gmail.com>
2020-04-20 09:36:01 +0800

    add fit query

--- streams/resize.ts
+++ streams/resize.ts
@@ -1,8 +1,13 @@
 import sharp from "../utils/sharp";
-
+import {FitEnum} from 'sharp';
+const knownFits = ['cover', 'contain', 'fill', 'inside', 'outside'] as const;
 export default (
   width: number | undefined,
-  height: number | undefined
+  height: number | undefined,
+  fit: keyof FitEnum = 'fill'
 ): NodeJS.ReadableStream => {
-  return sharp().resize({ width, height, fit: 'fill' });
+  if(!knownFits.includes(fit)) {
+    fit = 'fill';
+  }
+  return sharp().resize({ width, height, fit });
 };
